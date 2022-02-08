78a6cdea1a3409811de92f6a66d91fe46871139b
Amirhossein Afshinfard <31241012+afshinfard@users.noreply.github.com>
2022-02-07 18:29:09 -0800

    Update conda_env.sh

--- hist/conda_env.sh
+++ hist/conda_env.sh
@@ -1,8 +1,21 @@
+## How to:
+
+* Activating an environment:
+
+```bash
+> conda_env "name of env"
+```
 
 
+## Environments: 
 
 * rust-env:
+
+Rust installed
+Rust mdbg
+
 ```bash
+> conda install -c conda-forge/label/rust_dev rust
 > conda install -c pkgw-forge cargo
 ```
 
