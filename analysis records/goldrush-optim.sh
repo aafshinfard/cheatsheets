7c56dcb736d77d0fe77b4a041dfe17b72a180909
Amirhossein Afshinfard <31241012+aafshinfard@users.noreply.github.com>
2024-02-27 23:46:49 -0800

    Update goldrush-optim.sh

--- analysis records/goldrush-optim.sh
+++ analysis records/goldrush-optim.sh
@@ -34,13 +34,21 @@ rounds=6
 
 command time -v -o time.goldrush-run.time goldrush run reads=${reads} G=3e9 t=$t z=$z tile=${tile} b=$b u=$u a=$a o=$o x=$x m=$m M=$M r=$r P=$P d=$d span=${span} dist=${dist} k_ntLink=${k_ntLink} w_ntLink=${w_ntLink} rounds=${rounds} > log.goldrush-run.log 2>&1
 
+conda_env deepcut
+
 goldrush=$(ls -t *fa | head -n1)
 ln -s ${goldrush} goldrush.fa
 
 fasta=goldrush.fa
 ref=grch38.fa
 
+quast-lg -t48 -es --fast --large --scaffold-gap-max-size 100000 --min-identity 80 --split-scaffolds -R ${ref} -o quast_${fasta}_${ref}_quast ${fasta}
+minimap2 --version > quast_${fasta}_${ref}_quast/minimap2.version
+quast --version > quast_${fasta}_${ref}_quast/quast.version
+cat quast_${fasta}_${ref}_quast/transposed_report.tsv |  mlr --tsv cut -o -f Assembly,NG50,NGA50,"# misassemblies","# local misassemblies","Genome fraction (%)","Duplication ratio","Total length","Unaligned length","# unaligned contigs","# N's per 100 kbp","# mismatches per 100 kbp","# indels per 100 kbp"| sed 's/\t/|/g' | sed 's/^/|/g' | sed -E ':a;s/([0-9]+)([0-9]{3}\>)/\1,\2/g;ta' > quast_${fasta}_${ref}_quast.tsv
+cat quast_${fasta}_${ref}_quast.tsv
 
+conda_env goldrush
 
 
 ######################################
