b3659763e4e09c7fdd8c3ac178b8d9be70658388
Amirhossein Afshinfard <31241012+aafshinfard@users.noreply.github.com>
2024-02-27 23:39:27 -0800

    Update goldrush-optim.sh

--- analysis records/goldrush-optim.sh
+++ analysis records/goldrush-optim.sh
@@ -1,7 +1,17 @@
 # README content 1 (set 1)
 Goldrush parameters used for these experiments:
 # set by Amirhossein
-reads=
+
+# ln -s /projects/btl/datasets/hprc/HG02055/nanopore/HG02055.fq hg02055.ont.fq
+# ln -s /projects/btl/datasets/hprc/HG01243/nanopore/HG01243.fq hg01243.ont.fq
+# ln -s /projects/btl/datasets/hsapiens/giab/AshkenazimTrio/HG002_NA24385_son/nanopore/gm_2020.11_guppy5/NA24385.ont.fq hg002.ont.fq
+
+# ln -s /projects/btl/reference_genomes/H_sapiens/GRCh38/GCA_000001405.15_GRCh38_genomic.chr-only.fa grch38.fa
+
+reads=hg02055.ont.fq
+reads=hg01243.ont.fq
+reads=hg002.ont.fq
+
 t=45
 # optimization work
 z=2000
@@ -23,6 +33,14 @@ w_ntLink=250
 rounds=6
 
 command time -v -o time.goldrush-run.time goldrush run reads=${reads} G=3e9 t=$t z=$z tile=${tile} b=$b u=$u a=$a o=$o x=$x m=$m M=$M r=$r P=$P d=$d span=${span} dist=${dist} k_ntLink=${k_ntLink} w_ntLink=${w_ntLink} rounds=${rounds} > log.goldrush-run.log 2>&1
+
+goldrush=$(ls -t *fa | head -n1)
+ln -s ${goldrush} goldrush.fa
+
+fasta=goldrush.fa
+ref=grch38.fa
+
+
 
 
 ######################################
